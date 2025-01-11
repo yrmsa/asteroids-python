e6613dc2e48ad50e2b6dff0842f137e962820a18
yrmsa <yrmsa>
2025-01-11 20:45:23 +0700

    added delta time

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
 
+  delta = pygame.time.Clock()
+  dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
   while True:
@@ -17,6 +19,7 @@ def main():
       
     screen.fill("#000000")
 
+    dt = delta.tick(60) / 1000
     pygame.display.flip()
 
 
